667abc8bd2c61a13e494040f981945a3331a1320
MystixWolf <themystixwolf@gmail.com>
2022-01-18 13:47:08 -0800

    test4

--- chunkminer.lua
+++ chunkminer.lua
@@ -5,39 +5,57 @@ function mine()
     end
 end
 
+function chunkminer(direction)
+    if (direction == "left") then
+        test = 1 -- odd is left and even is right
+        for i = 1, 15 do
+            mine()
+            if ((test % 2) == 0) then -- if even
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
+                test = test + 1
+            else
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+                test = test + 1
+            end
+        end
+        mine()
+        turtle.digDown()
+        turtle.Down()
+        turtle.turnLeft()
+    elseif (direction == "right") then
+        test = 1 -- odd is left and even is right
+        for i = 1, 15 do
+            mine()
+            if ((test % 2) == 0) then -- if even
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
+                test = test + 1
+            else
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+                test = test + 1
+            end
+        end
+        mine()
+        turtle.digDown()
+        turtle.Down()
+        turtle.turnRight()
+    end
+end
+
 io.write('Which direction is the chunk(left or right)\n')
 input = io.read()
 
-if (input == "left") then
-    test = 1 -- odd is left and even is right
-    for i = 1, 16 do
-        mine()
-        if ((test % 2) == 0) then -- if even
-            turtle.turnRight()
-            turtle.forward()
-            turtle.turnRight()
-            test = test + 1
-        else
-            turtle.turnLeft()
-            turtle.forward()
-            turtle.turnLeft()
-            test = test + 1
-        end
-    end
-elseif (input == "right") then
-    test = 1 -- odd is left and even is right
-    for i = 1, 16 do
-        mine()
-        if ((test % 2) == 0) then -- if even
-            turtle.turnRight()
-            turtle.forward()
-            turtle.turnRight()
-            test = test + 1
-        else
-            turtle.turnLeft()
-            turtle.forward()
-            turtle.turnLeft()
-            test = test + 1
-        end
-    end
+while true do
+    chunkminer(input)
 end
