4487fb8ebf28d20b92eb9e561cbc33dc5a818fd5
MystixWolf <themystixwolf@gmail.com>
2022-01-18 13:01:25 -0800

    test

--- chunkminer.lua
+++ chunkminer.lua
@@ -1,7 +1,29 @@
 function mine()
-    for i = 1, 16 do 
+    for i = 1, 15 do 
         turtle.dig()
         turtle.forward()
     end
+end
 
-mine()
+function inspect(direction)
+    if (direction == "left") then
+        turtle.turnLeft()
+        result = turtle.inspect()
+        turtle.turnRight()
+        return result
+    elseif (direction == "right") then
+        turtle.turnRight()
+        result = turtle.inspect()
+        turtle.turnLeft()
+        return result
+    end
+    
+end
+
+--for i = 1, 16 do
+--    mine()
+--end
+
+io.write('Which direction is to mine (left or right)')
+input = io.read()
+io.write(input)
